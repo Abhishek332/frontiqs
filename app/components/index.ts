b65d3c5fb866f6c78bf168fcd06ba6b84ea10232
abhishek332 <itsabhiofficial.21@gmail.com>
2024-11-24 14:52:30 +0530

    add exports for some components in index file

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,4 +1,10 @@
 export { default as Button } from './button/Button';
 export { default as Card } from './card/Card';
+export { default as Input } from './input-fields/InputField';
+export { default as Modal } from './modal/Modal';
 export { default as Navbar } from './navbar/NavBar';
+export { default as ProgressBar } from './progress-bar/ProgressBar';
 export { default as SideBar } from './sidebar/SideBar';
+export { default as Table } from './table/Table';
+export { default as ToggleSwitch } from './toggle-switch/ToggleSwitch';
+export { default as Tooltip } from './tooltip/Tooltip';
