8f17c286d36599be785ab94ca3b01a56801e15d7
abhishek332 <itsabhiofficial.21@gmail.com>
2024-12-15 21:54:58 +0530

    minor change

--- app/code-analyzer/page.tsx
+++ app/code-analyzer/page.tsx
@@ -43,7 +43,6 @@ const CodeAnalyzer: React.FC = () => {
       <FileUpload onFileUpload={(selectedFile) => setFile(selectedFile)} />
       <Button
         onClick={handleAnalyze}
-        disabled={false}
         text='Analyze Code'
       />
 
